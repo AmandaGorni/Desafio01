70f07f558d53530be59216050b614931a66ec9f6
AmandaGorni <ap.gorni@gmail.com>
2021-05-17 00:11:33 -0300

    desafio 01 finalizado

---
+++ index.js
@@ -0,0 +1,11 @@
+const name = "Amanda"
+const age = 30
+const year = 2021
+
+console.log(`Meu ano de nascimento é ${year-age}`)
+
+let iamhuman = true
+let value = 20
+let value2 = 30
+
+console.log(value+value2)
